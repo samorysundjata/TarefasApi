8f39cabfc2b03e6dcf9e933cf83e425815093b3f
Samory Sundjata <samory.sundjata@gmail.com>
2023-03-16 13:36:31 -0300

    Criando o AddPersistence

--- TarefasApi/Program.cs
+++ TarefasApi/Program.cs
@@ -1,6 +1,11 @@
+using TarefasApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddPersistence();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapTarefasEndpoints();
 
 app.Run();
